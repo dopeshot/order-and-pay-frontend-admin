03ba9072e6f802a897437b6ac8aa7236000eecf1
Lars Flieger <a.purenetwork@gmail.com>
2021-11-01 16:38:18 +0100

    Fix issue with missing key

--- src/pages/Tables/Tables.tsx
+++ src/pages/Tables/Tables.tsx
@@ -10,17 +10,13 @@ export const Tables: React.FunctionComponent = () => {
             syncTables()
     }, [syncTables, hasLoadedTablesOnce])
 
-    function refreshTables() {
-        syncTables()
-    }
-
     return (
         <div className="container mx-auto">
             {isLoadingTables ? <p><svg className="inline animate-spin -ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>Loading tables...</p> : (<>
-                <button onClick={() => refreshTables()} className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Refresh</button>
+                <button onClick={() => syncTables()} className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Refresh</button>
                 <table className="table-fixed">
                     <thead className="bg-indigo-200">
                         <tr>
@@ -32,7 +28,7 @@ export const Tables: React.FunctionComponent = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {tables.map((table, index) => (<tr>
+                        {tables.map((table, index) => (<tr key={index}>
                             <td>{table.id}</td>
                             <td className="text-right">{table.tableNumber}</td>
                             <td className="text-right">{table.capacity}</td>
