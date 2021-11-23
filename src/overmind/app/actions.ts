b534a224c275320b22483eccdd40c5730675fe45
Lars Flieger <a.purenetwork@gmail.com>
2021-11-23 18:26:47 +0100

    Ref state listener for mobile

--- src/overmind/app/actions.ts
+++ src/overmind/app/actions.ts
@@ -1,10 +1,12 @@
 import { Context } from ".."
 
-export const onInitializeOvermind = ({ state }: Context): void => {
-    window.addEventListener("resize", () => {
-        state.app.isMobile = window.innerWidth < 768
-    })
-} 
+export const onInitializeOvermind = ({ state, actions }: Context): void => {
+    window.addEventListener("resize", actions.app.checkIsMobile)
+}
+
+export const checkIsMobile = ({ state }: Context): void => {
+    state.app.isMobile = window.innerWidth < 768
+}
 
 export const toggleSidebar = ({ state }: Context): void => {
     state.app.layoutIsSideBarOpen = !state.app.layoutIsSideBarOpen
