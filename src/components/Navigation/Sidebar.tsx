f576b7db988074aabe34c5d3a9a5daf829138b9a
Lars Flieger <a.purenetwork@gmail.com>
2021-11-23 16:38:36 +0100

    Fix bug with animation (where color faded in to slowly when you switch menus)

--- src/components/Navigation/Sidebar.tsx
+++ src/components/Navigation/Sidebar.tsx
@@ -31,18 +31,18 @@ export const Sidebar: React.FunctionComponent = () => {
     }]
 
     return (
-        <nav className={`sidebar transition duration-1000 ease-in-out ${!layoutIsSideBarOpen && `sidebar-closed`}`}>
+        <nav className={`sidebar transition duration-500 ease-in-out ${!layoutIsSideBarOpen && `sidebar-closed`}`}>
             {sidebarContent.map((sidebarItem, i) => <div key={i} className="text-darkgrey mt-16">
                 <div className="mx-6">
                     <h5 className="sidebar-heading text-darkgrey text-xs uppercase font-semibold tracking-wider border-b border-lightgrey">
-                        <span className={`bg-white-lightgrey pr-3 transition duration-1000 ease-in-out ${layoutIsSideBarOpen ? `opacity-100` : `opacity-0`}`}>{sidebarItem.title}</span>
+                        <span className={`bg-white-lightgrey pr-3 transition duration-500 ease-in-out ${layoutIsSideBarOpen ? `opacity-100` : `opacity-0`}`}>{sidebarItem.title}</span>
                     </h5>
                 </div>
                 <ul>
                     {sidebarItem.items.map((sidebarAtom, i) => <li key={i}>
                         <NavLink to={sidebarAtom.path} className="whitespace-nowrap block border-l-4 border-transparent hover:bg-active-grey w-full h-full py-2 px-5">
                             <FontAwesomeIcon icon={sidebarAtom.icon} className="text-lg text-center mr-5" style={{ width: "24px" }}></FontAwesomeIcon>
-                            <span className={`transition duration-1000 ease-in-out ${layoutIsSideBarOpen ? `opacity-100` : `opacity-0`}`}>{sidebarAtom.title}</span>
+                            <span className={`transition-opacity duration-500 ease-in-out ${layoutIsSideBarOpen ? `opacity-100` : `opacity-0`}`}>{sidebarAtom.title}</span>
                         </NavLink>
                     </li>)}
                 </ul>
