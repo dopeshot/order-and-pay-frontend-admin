1b424973cc8a8c338208b4bd5bfafd8f6c034e88
Lars Flieger <a.purenetwork@gmail.com>
2021-12-16 19:38:23 +0100

    Update button

--- src/components/Table/AddTableModal.tsx
+++ src/components/Table/AddTableModal.tsx
@@ -1,4 +1,4 @@
-import { faCheck } from "@fortawesome/free-solid-svg-icons"
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons"
 import { Field, Form, Formik } from "formik"
 import * as yup from 'yup'
 import { useActions, useAppState } from "../../overmind"
@@ -19,7 +19,7 @@ export const AddTableModal: React.FunctionComponent<TableModalProps> = (props) =
 
     const initialValues = {
         tableNumber: "",
-        capacity: 3
+        capacity: ""
     }
 
     const addTableSchema = yup.object().shape({
@@ -27,7 +27,7 @@ export const AddTableModal: React.FunctionComponent<TableModalProps> = (props) =
         capacity: yup.number().required("Capacity must be defined").min(1, "Capacity must be greater than 1").max(100, "Capacity cannot be greater than 100")
     })
 
-    const submitForm = (values: typeof initialValues) => {
+    const submitForm = (values: any) => {
         createTable({
             ...values,
             setDisplayModal: props.setDisplayModal
@@ -55,17 +55,17 @@ export const AddTableModal: React.FunctionComponent<TableModalProps> = (props) =
 
                                     {/* Tablenumber Input with Label */}
                                     <label className="block text-darkgrey text-sm font-semibold pb-2" htmlFor="tablenumber">Tischnummer</label>
-                                    <Field type="text" data-cy="table-modal-tablenumber-input" name="tableNumber" id="tableNumber" placeholder="A1" className={`font-roboto rounded-xl w-full pl-4 py-2 sm:w-1/2 ${errors.tableNumber && touched.tableNumber ? 'bg-danger-red bg-opacity-10 border-2 border-danger-red mb-2' : 'border border-border-grey mb-5'}`} />
+                                    <Field type="text" data-cy="table-modal-tablenumber-input" name="tableNumber" id="tableNumber" placeholder="A1" className={`font-roboto rounded-xl w-full pl-4 py-2 sm:w-1/2 ${errors.tableNumber && touched.tableNumber ? 'bg-danger-red bg-opacity-10 border-2 border-danger-red mb-2 focus:outline-none focus:border-danger-red focus:ring-danger-red' : 'border border-border-grey mb-5'}`} />
                                     <FormError dataCy="table-modal-tablenumber-input-error" field="tableNumber" />
 
                                     {/* Capacity Input with Label */}
                                     <label className="block text-darkgrey text-sm font-semibold pb-2" htmlFor="tablenumber">Personenanzahl</label>
-                                    <Field type="number" data-cy="table-modal-capacity-input" name="capacity" id="capacity" placeholder="2" className={`font-roboto rounded-xl w-full pl-4 py-2 sm:w-1/2 sm:mb-0 ${errors.capacity && touched.capacity ? 'bg-danger-red bg-opacity-10 border-2 border-danger-red' : 'border border-border-grey mb-3'}`} />
+                                    <Field type="number" data-cy="table-modal-capacity-input" name="capacity" id="capacity" placeholder="2" className={`font-roboto rounded-xl w-full pl-4 py-2 sm:w-1/2 sm:mb-0 ${errors.capacity && touched.capacity ? 'bg-danger-red bg-opacity-10 border-2 border-danger-red focus:outline-none focus:border-danger-red focus:ring-danger-red' : 'border border-border-grey mb-3'}`} />
 
                                     {/* Capacity Quick */}
                                     <div className="flex mb-2 sm:inline-flex sm:justify-between sm:w-1/2 sm:pl-3">
                                         {capacityTemplates.map((capacityTemplate, index) => (
-                                            <button type="button" data-cy={`capacity-quick-${capacityTemplate}`} key={`ppl_${index}${capacityTemplate}`} onClick={() => setFieldValue('capacity', capacityTemplate, false)} className={`flex justify-center items-center hover:bg-primary-blue-hover focus:bg-primary-blue-hover focus:shadow-focus hover:text-white focus:text-white border border-primary-blue ${values.capacity === capacityTemplate ? `bg-primary-blue text-white` : `text-primary-blue`} rounded-full mr-4 sm:mr-0`} style={{ minWidth: "50px", minHeight: "50px" }}>
+                                            <button type="button" data-cy={`capacity-quick-${capacityTemplate}`} key={`ppl_${index}${capacityTemplate}`} onClick={() => setFieldValue('capacity', capacityTemplate)} className={`flex justify-center items-center hover:bg-primary-blue-hover focus:bg-primary-blue-hover focus:shadow-focus hover:text-white focus:text-white border border-primary-blue ${values.capacity as unknown as number === capacityTemplate ? `bg-primary-blue text-white` : `text-primary-blue`} rounded-full mr-4 sm:mr-0`} style={{ minWidth: "50px", minHeight: "50px" }}>
                                                 <p className="font-semibold font-roboto">{capacityTemplate}</p>
                                             </button>
                                         ))}
@@ -75,7 +75,7 @@ export const AddTableModal: React.FunctionComponent<TableModalProps> = (props) =
                                 </div>
                                 <div className="flex flex-col sm:flex sm:flex-row-reverse sm:justify-between">
                                     {/* Save and Cancel Buttons */}
-                                    <PrimaryButton dataCy="table-save" disabled={!(dirty && isValid)} type="submit" icon={faCheck}>Speichern</PrimaryButton>
+                                    <PrimaryButton dataCy="table-save" disabled={!(dirty && isValid)} type="submit" icon={(dirty && isValid) ? faCheck : faTimes}>Speichern</PrimaryButton>
                                     <SecondaryButton dataCy="table-cancel" content="Abbrechen" onClick={() => props.setDisplayModal(false)} />
                                 </div>
                             </Form>
