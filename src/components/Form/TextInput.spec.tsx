b5dcad777066a34a7c625ce6bbf7cd605e984ecd
Lars Flieger <a.purenetwork@gmail.com>
2022-01-23 17:01:57 +0100

    Add Wrapper to component tests

--- src/components/Form/TextInput.spec.tsx
+++ src/components/Form/TextInput.spec.tsx
@@ -17,6 +17,18 @@ const schema = yup.object().shape({
     title: yup.string().required("Title is required"),
 })
 
+const Wrapper: React.FC = ({ children }) => {
+    return <Formik initialValues={initialValues} onSubmit={submitForm} validationSchema={schema} >
+        {(formik) => (
+            <Form>
+                <div className="w-80">
+                    {children}
+                </div>
+            </Form>
+        )}
+    </Formik>
+}
+
 describe('Text Input', () => {
     const data = {
         name: "title",
@@ -27,15 +39,9 @@ describe('Text Input', () => {
     }
 
     beforeEach(() => {
-        mount(<Formik initialValues={initialValues} onSubmit={submitForm} validationSchema={schema} >
-            {(formik) => (
-                <Form>
-                    <div className="w-80">
-                        <TextInput name={data.name} placeholder={data.placeholder} labelText={data.labelText} helperText={data.helperText} />
-                    </div>
-                </Form>
-            )}
-        </Formik>)
+        mount(<Wrapper>
+            <TextInput name={data.name} placeholder={data.placeholder} labelText={data.labelText} helperText={data.helperText} />
+        </Wrapper>)
     })
 
     it('should set name in input attribute', () => {
@@ -52,15 +58,9 @@ describe('Text Input', () => {
 
     it('should set label star when label required', () => {
         cy.get('label').should('not.contain', '*')
-        mount(<Formik initialValues={initialValues} onSubmit={submitForm} validationSchema={schema} >
-            {(formik) => (
-                <Form>
-                    <div className="w-80">
-                        <TextInput name={data.name} placeholder={data.placeholder} labelText={data.labelText} labelRequired />
-                    </div>
-                </Form>
-            )}
-        </Formik>)
+        mount(<Wrapper>
+            <TextInput name={data.name} placeholder={data.placeholder} labelText={data.labelText} labelRequired />
+        </Wrapper>)
         cy.get('label').should('contain', '*')
     })
 
@@ -76,15 +76,9 @@ describe('Text Input', () => {
     })
 
     it('should have icon at the end when set icon', () => {
-        mount(<Formik initialValues={initialValues} onSubmit={submitForm} validationSchema={schema} >
-            {(formik) => (
-                <Form>
-                    <div className="w-80">
-                        <TextInput name={data.name} placeholder={data.placeholder} labelText={data.labelText} icon={faEuroSign} />
-                    </div>
-                </Form>
-            )}
-        </Formik>)
+        mount(<Wrapper>
+            <TextInput name={data.name} placeholder={data.placeholder} labelText={data.labelText} icon={faEuroSign} />
+        </Wrapper>)
         cy.get('svg').should('be.visible')
         cy.get('svg').should('have.class', 'fa-euro-sign')
     })
@@ -94,15 +88,9 @@ describe('Text Input', () => {
     })
 
     it('should have type email when set type', () => {
-        mount(<Formik initialValues={initialValues} onSubmit={submitForm} validationSchema={schema} >
-            {(formik) => (
-                <Form>
-                    <div className="w-80">
-                        <TextInput name={data.name} type="email" placeholder={data.placeholder} labelText={data.labelText} icon={faEuroSign} />
-                    </div>
-                </Form>
-            )}
-        </Formik>)
+        mount(<Wrapper>
+            <TextInput name={data.name} type="email" placeholder={data.placeholder} labelText={data.labelText} icon={faEuroSign} />
+        </Wrapper>)
         cy.get('input').invoke('attr', 'type').should('contain', 'email')
     })
 
