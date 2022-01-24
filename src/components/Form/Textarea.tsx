3fc62cf891c64da37a09eccc2fa2bef7f982e8dd
Lars Flieger <a.purenetwork@gmail.com>
2022-01-24 17:00:18 +0100

    Add textarea

---
+++ src/components/Form/Textarea.tsx
@@ -0,0 +1,43 @@
+import { Field, FieldProps } from "formik"
+import { FormError } from "../Errors/FormError"
+
+type TextareaProps = {
+    /** Gives the input a unique name */
+    name: string
+    /** Specify the placeholder attribute for the <textarea> */
+    placeholder: string
+    /** Inform users what the corresponding input fields mean. */
+    labelText: string
+    /** When set Required * will be seen  */
+    labelRequired?: boolean
+    /** Provide text that is used alongside the control label for additional help. */
+    helperText?: string
+    /** Specify the maximum length of the string in <textarea> */
+    maxLength?: number
+    /** Specify the rows attribute for the <textarea> */
+    rows?: number
+}
+
+/**
+ * Textarea, can only be used with Formik
+ */
+export const Textarea: React.FC<TextareaProps> = ({ name, placeholder, labelText, labelRequired = false, helperText, maxLength, rows = 2 }) => {
+    return (
+        <div className={`mb-4`}>
+            <Field name={name}>{(props: FieldProps<any>) => (
+                <>
+                    <div className="flex justify-between">
+                        <label className="text-darkgrey text-sm font-semibold" htmlFor={name}>{labelText}{labelRequired && <span className="text-primary-blue ml-1">*</span>}</label>
+                        {maxLength && <span className="text-lightgrey">{props.field.value.length}/{maxLength}</span>}
+                    </div>
+                    <div>
+                        <textarea placeholder={placeholder} {...props.field} rows={rows} className={`font-roboto rounded-xl p-2 w-full ${props.meta.error && props.meta.touched ? 'bg-danger-red bg-opacity-10 border-2 border-danger-red focus:outline-none focus:border-danger-red focus:ring-danger-red' : 'border border-border-grey'}`}>
+                        </textarea>
+                    </div>
+                    {!(props.meta.error && props.meta.touched) && <p className="text-lightgrey">{helperText}</p>}
+                </>
+            )}</Field>
+            <FormError field={name} />
+        </div>
+    )
+}
