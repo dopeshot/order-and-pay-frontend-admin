e42a9aff458bfc9a62b9eb61ab3630afbb17c019
Lars Flieger <a.purenetwork@gmail.com>
2022-01-26 20:28:35 +0100

    Update dto

--- src/overmind/menus/effects.ts
+++ src/overmind/menus/effects.ts
@@ -1,11 +1,9 @@
 import { request } from "../../services/axios";
-import { Status } from "../../services/shared";
 import { Menu } from "./state";
 
 export type MenuDto = {
     title: string
     description: string
-    status: Status
     isActive: boolean
 }
 
