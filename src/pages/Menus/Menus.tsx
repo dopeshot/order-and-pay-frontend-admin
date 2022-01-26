e51ab5c746769de7d426c2e2f4107b164b77948c
Lars Flieger <a.purenetwork@gmail.com>
2022-01-26 19:15:18 +0100

    Add overview to menu page

--- src/pages/Menus/Menus.tsx
+++ src/pages/Menus/Menus.tsx
@@ -1,8 +1,32 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { faFolder, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { useEffect } from "react"
 import { Button } from "../../components/Buttons/Button"
+import { IconButton } from "../../components/Buttons/IconButton"
+import { List } from "../../components/UI/List"
+import { ListItem } from "../../components/UI/ListItem"
+import { Tag, TagTypesEnum } from "../../components/UI/Tag"
+import { useActions, useAppState } from "../../overmind"
 
 export const Menus: React.FC = () => {
+    // Get hooks to manipulate global state
+    const { getAllMenus } = useActions().menus
 
+    // Get global state
+    const { menus, isLoadingMenus } = useAppState().menus
+
+    // Load labels when page is loaded
+    useEffect((): void => {
+        getAllMenus()
+    }, [getAllMenus])
+
+    const handleDelete = async (event: any, id: string) => {
+        // This prevents the event from bubbling up the DOM to the parent node where you open edit
+        event.stopPropagation()
+        event.preventDefault()
+
+        // Delete the menu
+        console.log(id)
+    }
 
     return <div className="container md:max-w-full mt-12" >
         <div className="flex flex-col md:flex-row md:justify-between">
@@ -14,5 +38,12 @@ export const Menus: React.FC = () => {
                 <Button icon={faPlus} to="/menus/add">Menü hinzufügen</Button>
             </div>
         </div>
+        <List lines>
+            {menus.map((menu) => <ListItem key={menu._id} title={menu.title} icon={faFolder} to={`/menus/edit/${menu._id}`}>
+                {menu.isActive && <Tag title="Active" type={TagTypesEnum.green} />}
+                <IconButton className="ml-auto mr-4" icon={faTrash} onClick={(event) => handleDelete(event, menu._id)} />
+            </ListItem>)}
+        </List>
+
     </div>
 }
