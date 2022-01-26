a539bc45e270636c5befd21cca05e32775e587bd
Lars Flieger <a.purenetwork@gmail.com>
2022-01-26 14:24:14 +0100

    Add list labels

--- src/pages/Labels/Labels.tsx
+++ src/pages/Labels/Labels.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react"
+import { Button } from "../../components/Buttons/Button"
+import { useActions, useAppState } from "../../overmind"
+
 export const Labels: React.FC = () => {
+    // Get hooks to manipulate global state
+    const { getAllLabels } = useActions().labels
+
+    // Get global state
+    const { labels } = useAppState().labels
+
+    // Load labels when page is loaded
+    useEffect((): void => {
+        getAllLabels()
+    }, [getAllLabels])
 
     return <div className="container md:max-w-full mt-12">
         <h1>Labels Page</h1>
+        <Button kind="primary">Create Label</Button>
+        {labels.map((label) => <div key={label._id}>{label.title}</div>)}
     </div>
 }
