b35c652ad92f647769cf70e4f8e59eb4fde8ad0f
Lars Flieger <a.purenetwork@gmail.com>
2022-01-26 14:52:52 +0100

    Add round borders

--- src/components/UI/ListItem.tsx
+++ src/components/UI/ListItem.tsx
@@ -27,13 +27,13 @@ export const ListItem: React.FC<ListItemProps> = ({ title, icon = faFolder, inde
     return (
         <>
             {to ?
-                <Link to={to} className={`flex items-center py-4 pl-7 ${background ? "bg-white-lightgrey hover:bg-gray-200" : "hover:bg-white-lightgrey "} ${indent ? "pl-16" : ""}`}>
+                <Link to={to} className={`flex items-center py-4 pl-7 rounded-lg ${background ? "bg-white-lightgrey hover:bg-gray-200" : "hover:bg-white-lightgrey "} ${indent ? "pl-16" : ""}`}>
                     <FontAwesomeIcon icon={icon} className="text-lightgrey mr-4" />
                     <h4 className="text-lg text-headline-black font-semibold">{title}</h4>
                     {children}
                 </Link>
                 :
-                <button type={type} onClick={onClick} className={`flex items-center py-4 pl-7 w-full ${background ? "bg-white-lightgrey hover:bg-gray-200" : "hover:bg-white-lightgrey"} ${indent ? "pl-16" : ""}`}>
+                <button type={type} onClick={onClick} className={`flex items-center py-4 pl-7 rounded-lg w-full ${background ? "bg-white-lightgrey hover:bg-gray-200" : "hover:bg-white-lightgrey"} ${indent ? "pl-16" : ""}`}>
                     <FontAwesomeIcon icon={icon} className="text-lightgrey mr-4" />
                     <h4 className="text-lg text-headline-black font-semibold">{title}</h4>
                     {children}
