eab4dd4b55c00a3c1b9c79ce08508e70be01c673
Lars Flieger <a.purenetwork@gmail.com>
2022-01-26 15:04:32 +0100

    Ref List

--- src/components/UI/List.tsx
+++ src/components/UI/List.tsx
@@ -1,6 +1,9 @@
+import { ListItem } from './ListItem'
+
 type ListProps = {
     /** If it should have lines between elements set to true */
     lines?: boolean
+    children: React.ReactElement<typeof ListItem>[] | React.ReactElement<typeof ListItem> // This is not forcing it https://stackoverflow.com/questions/44475309/how-do-i-restrict-the-type-of-react-children-in-typescript-using-the-newly-adde
 }
 
 /**
