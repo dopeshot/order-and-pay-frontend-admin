c8f65ceeb0b0e150c6f3c6c80075ddab4a1247b9
Lars Flieger <a.purenetwork@gmail.com>
2022-02-01 11:40:34 +0100

    Add editing into menu editor

--- src/pages/Menus/MenuEditor.tsx
+++ src/pages/Menus/MenuEditor.tsx
@@ -1,6 +1,6 @@
 import { faArrowLeft, faCheck } from "@fortawesome/free-solid-svg-icons"
 import { Form, Formik } from "formik"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useHistory, useParams } from "react-router-dom"
 import * as Yup from "yup"
 import { Button } from "../../components/Buttons/Button"
@@ -9,6 +9,7 @@ import { TextInput } from "../../components/Form/TextInput"
 import { Toggle } from "../../components/Form/Toggle"
 import { useActions } from "../../overmind"
 import { MenuDto } from "../../overmind/menus/effects"
+import { Menu } from "../../overmind/menus/state"
 
 type Params = {
     id: string
@@ -17,16 +18,36 @@ type Params = {
 export const MenuEditor: React.FC = () => {
     const { id } = useParams<Params>()
     const history = useHistory()
-    const { createMenu } = useActions().menus
+    const { createMenu, getMenuById, updateMenu } = useActions().menus
 
     // Component States
     const [isLoadingSave, setIsLoadingSave] = useState(false)
     const [isEditing, setIsEditing] = useState(false)
+    const [menu, setMenu] = useState<Menu>()
+
+    useEffect((): void => {
+        async function loadMenu() {
+            // Check if we are editing an existing menu
+            if (!id)
+                return
+
+            // Fetch menu and set editing
+            setIsEditing(true)
+            const menu = await getMenuById(id)
+
+            // Check if menu exists
+            if (!menu)
+                return
+
+            setMenu(menu)
+        }
+        loadMenu()
+    })
 
     const initialValues: MenuDto = {
-        title: "",
-        description: "",
-        isActive: false
+        title: menu?.title ?? "",
+        description: menu?.description ?? "",
+        isActive: menu?.isActive ?? false
     }
     const validationSchema = Yup.object().shape({
         title: Yup.string().min(2).max(32).required("Title is required"),
@@ -39,19 +60,22 @@ export const MenuEditor: React.FC = () => {
 
         // Check if we are editing or creating a new menu
         if (isEditing) {
-
+            await updateMenu({
+                id,
+                menu: values
+            })
         } else {
             await createMenu(values)
-            history.push("/menus")
         }
 
+        history.push("/menus")
         setIsLoadingSave(false)
     }
 
     return <div className="container mt-12">
         <Button kind="tertiary" to="/menus" icon={faArrowLeft}>Zurück</Button>
         <h1 className="text-2xl text-headline-black font-semibold">Neus Menü</h1>
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={submitForm}>
+        <Formik initialValues={initialValues} enableReinitialize validationSchema={validationSchema} onSubmit={submitForm}>
             <Form>
                 <TextInput name="title" labelText="Titel" placeholder="Mittagskarte, Abendmenu,..." labelRequired autoFocus />
                 <Textarea rows={3} name="description" labelText="Beschreibung" placeholder="Beschreibung" maxLength={240} helperText="Diese Beschreibung wird in der Menü Übersicht angezeigt." />
