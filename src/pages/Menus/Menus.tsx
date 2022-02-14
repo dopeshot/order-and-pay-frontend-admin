c28d94a98800d0fc5b68745c5e7d12af8f750724
Lars Flieger <a.purenetwork@gmail.com>
2022-02-14 22:26:59 +0100

    Fix paddings

--- src/pages/Menus/Menus.tsx
+++ src/pages/Menus/Menus.tsx
@@ -60,10 +60,10 @@ export const Menus: React.FC = () => {
     }
 
     return <div className="container md:max-w-full mt-12" >
-        <div className="flex flex-col md:flex-row md:justify-between">
+        <div className="flex flex-col md:flex-row md:justify-between mb-4">
             <div>
                 <h1 className="text-2xl text-headline-black font-semibold">Alle Menüs</h1>
-                <p className="text-lightgrey mr-3 mb-4">{!isLoadingMenus ? menus.length : 0} Gesamt</p>
+                <p className="text-lightgrey mb-4 md:mb-0">{!isLoadingMenus ? menus.length : 0} Gesamt</p>
             </div>
             <div>
                 <Button icon={faPlus} to="/menus/add">Menü hinzufügen</Button>
