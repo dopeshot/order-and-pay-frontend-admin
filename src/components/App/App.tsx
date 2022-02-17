ff4c9dfbff2e400c4255128dca52c95108a42ba6
Lars Flieger <a.purenetwork@gmail.com>
2022-02-17 16:22:10 +0100

    Fix url

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -25,9 +25,9 @@ export const App: React.FunctionComponent = () => {
               <Route exact path="/home" component={Home} />
               <Route exact path="/menus" component={Menus} />
               <Route path="/menus/add" component={MenuEditor} />
-              <Route path="/menus/:id" component={MenuEditor} />
               <Route path="/menus/labels" component={Labels} />
               <Route path="/menus/allergens" component={Allergens} />
+              <Route path="/menus/:id" component={MenuEditor} />
               <Route exact path="/">
                 <Redirect to="/home" />
               </Route>
