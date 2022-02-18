5afa9fac0273bcf6c7c41e5d77d7e940a1f1978e
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 00:10:23 +0100

    Add get category by id action

--- src/overmind/categories/actions.ts
+++ src/overmind/categories/actions.ts
@@ -1,4 +1,5 @@
 import { Context } from "..";
+import { Category } from "../dishes/effects";
 import { CategoryDto } from "./effects";
 
 // Create a category
@@ -7,6 +8,18 @@ export const createCategory = async ({ effects }: Context, categoryDto: Category
         // We just await the creation no need to update category object
         await effects.categories.createCategory(categoryDto)
         return true
+    } catch (error) {
+        console.error(error)
+        throw (error)
+    }
+}
+
+// Get category by id action
+export const getCategoryById = async ({ state, effects }: Context, id: string): Promise<Category> => {
+    try {
+        const response = await effects.categories.getCategoryById(id)
+        const category = response.data
+        return category
     } catch (error) {
         console.error(error)
         throw (error)
