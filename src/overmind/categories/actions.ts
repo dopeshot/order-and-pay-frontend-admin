8f65f89b84744c08577ebe3de112b353c10cb901
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 22:20:16 +0100

    Add error notification to update categories

--- src/overmind/categories/actions.ts
+++ src/overmind/categories/actions.ts
@@ -23,7 +23,7 @@ export const createCategory = async ({ effects, actions }: Context, categoryDto:
 }
 
 // Get category by id action
-export const getCategoryById = async ({ state, effects }: Context, id: string): Promise<Category> => {
+export const getCategoryById = async ({ effects }: Context, id: string): Promise<Category> => {
     try {
         const response = await effects.categories.getCategoryById(id)
         const category = response.data
@@ -35,13 +35,20 @@ export const getCategoryById = async ({ state, effects }: Context, id: string): 
 }
 
 // Update menu by id action
-export const updateCategoryById = async ({ effects }: Context, { id, category }: { id: string, category: CategoryDto }): Promise<boolean> => {
+export const updateCategoryById = async ({ effects, actions }: Context, { id, category }: { id: string, category: CategoryDto }): Promise<true> => {
     try {
         // We just await the update no need to update menu object
         await effects.categories.updateCategory(id, category)
         return true
     } catch (error) {
         console.error(error)
+
+        actions.notify.createNotification({
+            title: "Fehler beim Erstellen der Kategorie",
+            message: axios.isAxiosError(error) && error.response ? error.response.data.message : "Netzwerk-Zeitüberschreitung",
+            type: "danger"
+        })
+
         throw (error)
     }
 }
