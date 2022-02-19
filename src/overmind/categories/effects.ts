047ef703e8ae43d054fef80599be3fd5b41a86c5
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 14:08:59 +0100

    Add delete effect

--- src/overmind/categories/effects.ts
+++ src/overmind/categories/effects.ts
@@ -35,3 +35,5 @@ export const createCategory = (categoryDto: CategoryDto) => request.post<Categor
 export const getCategoryById = (id: string) => request.get<Category>(`/categories/${id}`)
 // Update category by id
 export const updateCategory = (id: string, category: CategoryDto) => request.patch<Category>(`/categories/${id}`, category)
+// Delete category by id
+export const deleteCategory = (id: string) => request.delete(`/categories/${id}?type=hard`)
