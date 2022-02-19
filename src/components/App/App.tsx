616fdb1e150aed4c7fc1c2c4db7aef08bd1ced97
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 19:41:19 +0100

    Remove initializeUser from App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,11 @@
-import { useEffect } from 'react';
 import {
   BrowserRouter as Router, Redirect, Route, Switch
 } from 'react-router-dom';
-import { useActions } from '../../overmind';
 import { Login } from '../../pages/Login/Login';
 import { Admin } from './Admin';
 import { PrivateRoute } from './PrivateRoute';
 
 export const App: React.FunctionComponent = () => {
-  const { initializeUser } = useActions().auth
-
-  useEffect(() => {
-    initializeUser()
-  }, [initializeUser])
 
   return (
     <Router>
