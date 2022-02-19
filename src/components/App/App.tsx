8827d30643aa9294bcadcf4bb5233888c033810a
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 17:06:39 +0100

    Add nested routes for admin

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,18 +3,8 @@ import {
   BrowserRouter as Router, Redirect, Route, Switch
 } from 'react-router-dom';
 import { useActions } from '../../overmind';
-import { Allergens } from '../../pages/Allergens/Allergens';
-import { CategoryEditor } from '../../pages/Categories/CategoryEditor';
-import { Dishes } from '../../pages/Dishes/Dishes';
-import { Home } from '../../pages/Home/Home';
-import { Labels } from '../../pages/Labels/Labels';
 import { Login } from '../../pages/Login/Login';
-import { SingleMenu } from '../../pages/MenuOverview/SingleMenu';
-import { MenuEditor } from '../../pages/Menus/MenuEditor';
-import { Menus } from '../../pages/Menus/Menus';
-import { Tables } from '../../pages/Tables/Tables';
-import { Sidebar } from '../Navigation/Sidebar';
-import { Topbar } from '../Navigation/Topbar';
+import { Admin } from './Admin';
 
 export const App: React.FunctionComponent = () => {
   const { initializeUser } = useActions().auth
@@ -24,37 +14,15 @@ export const App: React.FunctionComponent = () => {
   }, [initializeUser])
 
   return (
-    <Router basename="/admin">
+    <Router>
       <Switch>
-        <>
-          <Route exact path="/login" component={Login} />
-          <div className="h-screen">
-            <Topbar />
-            <div className="flex" style={{ height: "calc(100vh - 64px)" }}>
-              <Sidebar />
-              <div className="flex-1 overflow-y-auto">
-                {/* Content Start */}
-                <Route exact path="/tables" component={Tables} />
-                <Route exact path="/home" component={Home} />
-                <Route exact path="/menus" component={Menus} />
-                <Route exact path="/menus/allergens" component={Allergens} />
-                <Route exact path="/menus/labels" component={Labels} />
-                <Route exact path="/menus/add" component={MenuEditor} />
-                <Route exact path="/menus/:menuId/categories" component={CategoryEditor} />
-                <Route exact path="/menus/:menuId/categories/:categoryId" component={CategoryEditor} />
-                <Route exact path="/menus/:menuId/edit" component={MenuEditor} />
-                <Route exact path="/menus/:menuId/categories/:categoryId/dish/:dishId" component={Dishes} />
-                <Route exact path="/menus/:menuId/categories/:categoryId/dish" component={Dishes} />
-                <Route exact path="/menus/:menuId/editor" component={SingleMenu} />
-                <Route exact path="/">
-                  <Redirect to="/home" />
-                </Route>
-                {/* Content End */}
-              </div>
-            </div>
-          </div>
-        </>
+        <Route path="/login" component={Login} />
+        <Route path="/admin" component={Admin} />
+        <Route path="/">
+          <Redirect to="login" />
+        </Route>
       </Switch>
-    </Router>
+    </Router >
   )
 }
+
