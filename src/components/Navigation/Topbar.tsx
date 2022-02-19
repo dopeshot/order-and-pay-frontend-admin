8e1c4de1bf5de972ea29d60c0c9d504cc6db31c4
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 18:05:12 +0100

    Add back to home for nav logo

--- src/components/Navigation/Topbar.tsx
+++ src/components/Navigation/Topbar.tsx
@@ -1,6 +1,7 @@
 import { faBars, faChevronDown, faSearch, faSignOutAlt } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
+import { Link, useRouteMatch } from "react-router-dom"
 import avatar from '../../img/avatar.png'
 import logo from '../../img/logo.png'
 import { useActions, useAppState } from "../../overmind"
@@ -16,13 +17,17 @@ export const Topbar: React.FunctionComponent = () => {
 
     const [dropdownOpen, setDropdownOpen] = useState(false)
 
+    const { url } = useRouteMatch()
+
     return (
         <div className="flex items-center bg-white shadow pr-3 md:pr-8 pl-5" style={{ height: "64px" }}>
             {/* Hamburger Button */}
             <IconButton dataCy="topbar-hamburger-button" icon={faBars} textColor="text-darkgrey" onClick={() => toggleSidebar()} className="mr-2 md:mr-4" />
 
             {/* Logo */}
-            <img src={logo} alt="logo" className="hidden md:block max-w-full mr-7" />
+            <Link to={`${url}/home`}>
+                <img src={logo} alt="logo" className="hidden md:block max-w-full mr-7" />
+            </Link>
 
             <div className="flex flex-grow justify-end md:justify-between">
                 {/* Search Desktop */}
