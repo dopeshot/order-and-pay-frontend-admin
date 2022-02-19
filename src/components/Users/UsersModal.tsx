68acc7bd8bd456640f1c38c4527be412b6996e1b
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 21:24:58 +0100

    Add comment

--- src/components/Users/UsersModal.tsx
+++ src/components/Users/UsersModal.tsx
@@ -69,7 +69,7 @@ export const UsersModal: React.FunctionComponent<UsersModalProps> = ({ modalEdit
                 setModalOpen(false)
             }
         } catch (error) {
-            console.error(error)
+            // Create or update failed. 
         } finally {
             setIsModalLoading(false)
         }
