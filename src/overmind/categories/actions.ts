5564b3618e2cc293f07dd54987bad2a2a03512aa
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 22:17:52 +0100

    Add error notification to create categories

--- src/overmind/categories/actions.ts
+++ src/overmind/categories/actions.ts
@@ -1,15 +1,23 @@
+import axios from "axios";
 import { Context } from "..";
 import { Category } from "../dishes/effects";
 import { CategoryDto } from "./effects";
 
 // Create a category
-export const createCategory = async ({ effects }: Context, categoryDto: CategoryDto): Promise<boolean> => {
+export const createCategory = async ({ effects, actions }: Context, categoryDto: CategoryDto): Promise<true> => {
     try {
         // We just await the creation no need to update category object
         await effects.categories.createCategory(categoryDto)
         return true
     } catch (error) {
         console.error(error)
+
+        actions.notify.createNotification({
+            title: "Fehler beim Erstellen der Kategorie",
+            message: axios.isAxiosError(error) && error.response ? error.response.data.message : "Netzwerk-Zeitüberschreitung",
+            type: "danger"
+        })
+
         throw (error)
     }
 }
