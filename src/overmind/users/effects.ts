f431a7171505f854d02d095d8ecb08b6e45cd590
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 17:12:52 +0100

    Add AccessToken type

--- src/overmind/users/effects.ts
+++ src/overmind/users/effects.ts
@@ -12,11 +12,14 @@ export type User = {
     username: string
     password?: string
 }
+export type AccessToken = {
+    accessToken: string
+}
 
 // Get all users
 export const getAllUser = () => request.get<User[]>('/users')
 // Create new user
-export const createUser = (user: UserDto) => request.post<User>('/auth/register', user)
+export const createUser = (user: UserDto) => request.post<AccessToken>('/auth/register', user)
 // Update user by id
 export const updateUser = (id: string, user: UserDto) => request.patch<User>(`/users/${id}`, user)
 // Delete user by id
