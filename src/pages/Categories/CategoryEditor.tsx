75ebbd3bd889df27c547c7df3812f89a5e680115
Lars Flieger <a.purenetwork@gmail.com>
2022-02-19 22:17:40 +0100

    Remove todo

--- src/pages/Categories/CategoryEditor.tsx
+++ src/pages/Categories/CategoryEditor.tsx
@@ -1,5 +1,4 @@
 import { faCheck, faCheckDouble, faCog, faEuroSign, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons"
-import axios from "axios"
 import { Form, Formik } from "formik"
 import { Fragment, useEffect, useState } from "react"
 import { useHistory, useParams } from "react-router-dom"
@@ -200,10 +199,7 @@ export const CategoryEditor: React.FunctionComponent = () => {
 
             history.push(`/admin/menus/${newCategory.menuId}/editor`)
         } catch (error) {
-            if (!axios.isAxiosError(error))
-                return
-
-            // MC: Put error display here (or we generalize it???)
+            // Create or update failed
         } finally {
             setIsLoadingSave(false)
         }
