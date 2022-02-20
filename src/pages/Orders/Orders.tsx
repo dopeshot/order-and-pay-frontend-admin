ed593f04e92cac7dba703def5a1a40785ca17153
Lars Flieger <a.purenetwork@gmail.com>
2022-02-20 14:32:43 +0100

    Update order

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -101,7 +101,7 @@ export const Orders: React.FC = () => {
             {/* Content */}
             {(orders.length === 0 && isLoadingOrders) ? <Loading /> : <List>
                 {orders.map((order) => <Fragment key={order._id}>
-                    <ListItem title={`#${order._id}`} icon="shopping-basket" background header={<><p className="pr-4">Tisch: {order.tableId}</p><Tag title={beautifyOrderStatus[order.Status].title} type={beautifyOrderStatus[order.Status].type} /></>}>
+                    <ListItem title={`#${order._id.slice(order._id.length - 3)}`} icon="shopping-basket" background header={<><p className="pr-4">Tisch: {order.tableId}</p><Tag title={beautifyOrderStatus[order.Status].title} type={beautifyOrderStatus[order.Status].type} /></>}>
                         <h6 className="text-headline-black text-lg font-semibold mr-3">{numberToPrice(order.price)}</h6>
                         <Button kind="tertiary" icon={faSync} className="text-darkgrey mr-4" onClick={() => updateOrderHandler(order._id, "edit")}>Wird Bearbeitet</Button>
                         <Button kind="tertiary" icon={faCheck} className="text-darkgrey mr-4" onClick={() => updateOrderHandler(order._id, "close")}>Abschließen</Button>
